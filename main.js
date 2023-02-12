c6ebd583777d540a2d36a65dda2d0c8ad3069db2
Rawle Juglal <RawleJuglal@users.noreply.github.com>
2023-02-11 20:28:31 -0600

    completed restaurant app
    features and no extras completed

--- main.js
+++ main.js
@@ -3,19 +3,29 @@ import { menuArray } from './data.js'
 
 const menuFeedEl = document.getElementById('menu-feed')
 const orderFeedEl = document.getElementById('order-feed')
+const modalEl = document.getElementById('modal');
 let userOrder = {
     pizza:0,
     hamburger:0,
     beer:0,
     isDisplayed:false,
+    cardInfo:{
+        name:'',
+        number:'',
+        ccv:''
+    }
 }
 
 document.addEventListener('click', (e)=>{
     if(e.target.dataset.item){
-        // console.log('we have a data item')
         handleAddClick(Number(e.target.dataset.item))
     } else if (e.target.dataset.remove){
         handleRemoveClick(e.target.dataset.remove.split('-')[2])
+    } else if (e.target.dataset.complete){
+        handleCompleteClick()
+    } else if (e.target.dataset.card){
+        e.preventDefault()
+        handlePayClick()
     }  else {
         console.log('we did not get an index')
     }
@@ -29,7 +39,7 @@ function getFeedHTML(){
         }).reduce((prevStr, currStr)=>{
             return prevStr += `, ${currStr}`;
         })
-        // console.log(ingredientStr)
+
         menuFeedHtml+=`<div id="${item.id}" class="--menu-item-container">
         <div id="item-img">
           <span id="item-emoji" class="item-emoji">${item.emoji}</span>
@@ -53,6 +63,10 @@ function renderFeed(){
 }
 
 function handleAddClick(value){
+    if(!document.getElementById('completion').classList.contains('hidden')){
+        toggleHidden(document.getElementById('completion'))
+    }
+
     if(value === 0){
         userOrder.pizza = userOrder.pizza +1;
     } else if (value === 1){
@@ -66,13 +80,8 @@ function handleAddClick(value){
 }
 
 function handleRemoveClick(value){
-    if(value === 'pizza'){
-        userOrder.pizza = 0;
-    } else if (value === 'hamburger'){
-        userOrder.hamburger = 0;
-    } else if (value === 'beer'){
-        userOrder.beer = 0;
-    }
+    userOrder[value] = 0
+
     if(!checkForAllFalsy()){
         renderOrder()
     }
@@ -81,7 +90,7 @@ function handleRemoveClick(value){
 
 function checkForAllFalsy(){
     if(userOrder.pizza === 0 &&  userOrder.hamburger === 0 && userOrder.beer === 0){
-        orderFeedEl.classList.toggle('hidden')
+        toggleHidden(orderFeedEl);
         userOrder.isDisplayed = false;
         return true;
     } else {
@@ -97,20 +106,11 @@ function renderOrder(){
     <div id="order-item-container" class="--order-items-container">`
     menuArray.forEach((item)=>{
         if(item.name === 'Pizza' && userOrder.pizza > 0){
-            const pizzaAmount = item.price * userOrder.pizza;
-            OrderFeedHtml+= `<div id="${item.name.toLowerCase()}-container" class="--order-item-container"><p id="order-item-${item.name}" class="--item-name">${item.name}</p>
-            <button id="order-btn" class="--order-btn-remove" data-remove="order-item-${item.name.toLowerCase()}">remove</button>
-            <p id="order-item-price" class="--item-price push-right">$${pizzaAmount}</p></div>`
+            OrderFeedHtml+= createOrderItemHtml(item)
         } else if(item.name === 'Hamburger' && userOrder.hamburger > 0){
-            const burgerAmount = item.price * userOrder.hamburger;
-            OrderFeedHtml+= `<div id="${item.name.toLowerCase()}-container" class="--order-item-container"><p id="order-item-${item.name}" class="--item-name">${item.name}</p>
-            <button id="order-btn" class="--order-btn-remove" data-remove="order-item-${item.name.toLowerCase()}">remove</button>
-            <p id="order-item-price" class="--item-price push-right">$${burgerAmount}</p></div>`
+            OrderFeedHtml+= createOrderItemHtml(item)
         } else if (item.name === 'Beer' && userOrder.beer > 0){
-            const beerAmount = item.price * userOrder.beer;
-            OrderFeedHtml+= `<div id="${item.name.toLowerCase()}-container" class="--order-item-container"><p id="order-item-${item.name}" class="--item-name">${item.name}</p>
-            <button id="order-btn" class="--order-btn-remove" data-remove="order-item-${item.name.toLowerCase()}">remove</button>
-            <p id="order-item-price" class="--item-price push-right">$${beerAmount}</p></div>`
+            OrderFeedHtml+= createOrderItemHtml(item)
         } else {
             console.log(`didn't have any ${item.name}`)
         }
@@ -121,15 +121,90 @@ function renderOrder(){
                         <p id="order-total" class="--order-total">Total price:</p>
                         <p id="order-amount" class="--order-amount push-right">$${totalPrice}</p>
                         </div>
-                        <button id="order-complete" class="--order-complete text-center">Complete Order</button>
+                        <button id="order-complete" class="--restaurant-btn text-center" data-complete="modal">Complete Order</button>
                     </div>`
-    // console.log(OrderFeedHtml)
+
     orderFeedEl.innerHTML = OrderFeedHtml
     
     if(!userOrder.isDisplayed){
-        orderFeedEl.classList.toggle('hidden')
+        toggleHidden(orderFeedEl);
         userOrder.isDisplayed = true;
     }
+}
+
+function createOrderItemHtml(item){
+    const itemAmount = item.price * Number(userOrder[item.name.toLowerCase()]);
+    const itemForFeed = `<div id="${item.name.toLowerCase()}-container" class="--order-item-container"><p id="order-item-${item.name}" class="--item-name">${item.name}</p>
+    <button id="order-btn" class="--order-btn-remove" data-remove="order-item-${item.name.toLowerCase()}">remove</button>
+    <p id="order-item-price" class="--item-price push-right">$${itemAmount}</p></div>`
+    return itemForFeed
+}
+
+function handleCompleteClick(){
+    toggleHidden(modalEl)
+}
+
+function handlePayClick(){
+    let passed = checkInputsMeetChecks()
+    passed ? saveCardInfo() : renderErrorMessage()
+    
+}
+
+function checkInputsMeetChecks(){
+    if(document.getElementById('cardInfo-name').value.length > 0 && Number.isInteger(Number(document.getElementById('cardInfo-number').value)) && document.getElementById('cardInfo-number').value.length === 16 && document.getElementById('cardInfo-ccv').value.length === 3 && Number.isInteger(Number(document.getElementById('cardInfo-ccv').value))){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function saveCardInfo(){
+    let cardName = document.getElementById('cardInfo-name')
+    let cardNumber = document.getElementById('cardInfo-number')
+    let cardCCV = document.getElementById('cardInfo-ccv')
+
+    let newCard = {
+        name:cardName.value,
+        number:cardNumber.value,
+        ccv:cardCCV.value,
+    }
+    userOrder.cardInfo = newCard;
+
+    //EMPTY CARD INFO
+    cardName.value = ''
+    cardNumber.value = ''
+    cardCCV.value = ''
+
+    //HIDES ERROR MESSAGE AGAIN IF THEY RESOLVE SECOND ATTEMPT
+    if(!document.getElementById('error-message').classList.contains('hidden')){
+        toggleHidden(document.getElementById('error-message'))
+    }
+
+    //RESET USERORDER ITEMS
+    resetUserOrderItems()
+
+    //SHOW COMPLETED ORDER DIV
+    toggleHidden(document.getElementById('completion'))
+    document.getElementById('completion-text').textContent = `Thanks ${userOrder.cardInfo.name}! Your order is on it's way`
+
+    //CLOSE MODAL
+    toggleHidden(document.getElementById('modal'))
+}
+
+function renderErrorMessage(){
+    toggleHidden(document.getElementById('error-message'))
+}
+
+function resetUserOrderItems(){
+    userOrder.pizza = 0;
+    userOrder.hamburger = 0;
+    userOrder.beer = 0;
+
+    orderFeedEl.innerHTML = ''
+}
+
+function toggleHidden(ele){
+    ele.classList.toggle('hidden')
 }
 
 renderFeed()
